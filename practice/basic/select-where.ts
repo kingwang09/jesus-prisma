eb099ebd2b3312beeab6cac44fc77a0adbafaedc
kingwang09 <kingwang09@gmail.com>
2023-10-24 11:47:46 +0900

    add where 조건식

---
+++ practice/basic/select-where.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+//이벤트 기반 로깅
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+  errorFormat: 'pretty',
+})
+
+prisma.$on('query', (e) => {
+  console.info('Query: ' + e.query)
+  console.info('Params: ' + e.params)
+  console.warn('Duration: ' + e.duration + 'ms')
+})
+
+async function main() {
+    let id = undefined;
+    let findUsers = await prisma.user.findMany({
+        where: {
+            id
+        }
+    });
+
+    console.log(findUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+//npx ts-node script.ts
+//npx prisma studio
