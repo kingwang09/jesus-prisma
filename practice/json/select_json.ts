045791db1f6e7780188e15df21189ddccfc74727
kingwang09 <kingwang09@gmail.com>
2023-10-26 10:01:04 +0900

    add select json

---
+++ practice/json/select_json.ts
@@ -0,0 +1,59 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+//기본 로깅
+//const prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error'],})
+
+//이벤트 기반 로깅
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+  errorFormat: 'pretty',
+})
+
+prisma.$on('query', (e) => {
+  console.info('Query: ' + e.query)
+  console.info('Params: ' + e.params)
+  console.warn('Duration: ' + e.duration + 'ms')
+})
+
+async function main() {
+    let foundedUsers = await prisma.user.findMany({
+      where: {
+        extendedPetsData: {
+          path: ['pet1', 'petName'],
+          equals: 'Claudine'
+        }
+      }
+    });
+
+    console.log(foundedUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+//npx ts-node script.ts
+//npx prisma studio
